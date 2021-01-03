13c8326dd7c94a3e8f9e232af6c06f1cc9d4dda2
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2021-01-03 16:35:56 -0700

    changed the .git to parent folder

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+
+
+import './index.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">amazona</a>
+          </div>
+          <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
