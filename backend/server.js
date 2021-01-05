004fc23971b529769c767d66202a4009bc1bd6ac
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2021-01-04 20:09:17 -0700

    added redux to productDetails component

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 import express from 'express';
 import data from './data.js';
 
+
 const app = express();
+
+
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message: 'Product not found' });
+    }
+
+});
+
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
@@ -10,7 +24,9 @@ app.get('/api/products', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Server is ready Max');
 });
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
 });
