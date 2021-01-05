f915ee218ecb38abcfda6eb3c877ec3caf2e29ae
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2021-01-05 13:10:50 -0700

    connected to MongoDB and seeded sample 2 users

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
 app.get('/api/products/:id', (req, res) => {
     const product = data.products.find((x) => x._id === req.params.id);
@@ -20,6 +28,12 @@ app.get('/api/products/:id', (req, res) => {
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
+});
+
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
 });
 
 app.get('/', (req, res) => {
