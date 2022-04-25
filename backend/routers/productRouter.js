49455907ce6f0f0f7cbe1c4d8c1492539e475d49
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-04-24 22:22:24 -0600

    create product handler on backend

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,7 +2,7 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import Product from '../models/productModel.js';
-import { isAuth } from '../utils.js';
+import { isAdmin, isAuth } from '../utils.js';
 const productRouter = express.Router();
 
 productRouter.get(
@@ -64,6 +64,27 @@ productRouter.post(
       } else {
         res.status(404).send({ message: 'Product Not Found' });
       }
+    })
+  );
+
+  productRouter.post(
+    '/',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+      const product = new Product({
+        name: 'samle name ' + Date.now(),
+        image: '/images/p1.jpg',
+        price: 0,
+        category: 'sample category',
+        brand: 'sample brand',
+        countInStock: 0,
+        rating: 0,
+        numReviews: 0,
+        description: 'sample description',
+      });
+      const createdProduct = await product.save();
+      res.send({ message: 'Product Created', product: createdProduct });
     })
   );
 
