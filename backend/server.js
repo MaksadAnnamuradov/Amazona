526ca1ac3efa238db2a4d5f3c7e11fe5b453d5e0
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-04-25 23:36:14 -0600

    added upload image functionality

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 // import data from './data.js';
 import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
+import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config();
 const app = express();
@@ -26,9 +28,15 @@ app.use('/api/products', productRouter);
 
 app.use('/api/orders', orderRouter);
 
+app.use('/api/uploads', uploadRouter);
+
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
 });
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 
 app.get('/', (req, res) => {
     res.send('Server is ready Max');
